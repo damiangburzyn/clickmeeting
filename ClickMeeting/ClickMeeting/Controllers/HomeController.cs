ca368d57ee6831df0dd08e73b6b9967373c08d26
robert845 <108308612+robert845@users.noreply.github.com>
2022-06-28 23:11:26 +0200

    drobne zmiany: wydlubanie roomId i Password z pokoju konferencji

--- ClickMeeting/ClickMeeting/Controllers/HomeController.cs
+++ ClickMeeting/ClickMeeting/Controllers/HomeController.cs
@@ -23,27 +23,38 @@ namespace ClickMeeting.Controllers
 
         public async Task<IActionResult> Index()
         {
+            string meetingName = "clickmeeting-test";
 
             string resultUrl = string.Empty;
-
             var email = "damian.gburzyn@gmail.com";
-            var roomId = "6674551";
-            var password = "clickmeeting";
-            var token = string.Empty;
+            //tego ponizej nie musimy już hardkodowac, wydłubiemy z Room
+            //var roomId = "6674551";
+            //var password = "clickmeeting";
+            //var token = string.Empty;
             string username = "testowyUser";
             try
             {
-                var conferences = await _cmClient.GetConferences(ConferenceStatus.Active, 1);
-                var result = await _cmClient.GetAutologinHash(roomId, email, username, password, token);
-                var conference = conferences.FirstOrDefault(a => a.Name == "clickmeeting-test");
-                //mały sukces TEN URL MOŻNA PODAĆ JAKO IFRAME !!!!!!!!!!!!!!! :)
-                resultUrl = _cmClient.GetAutologinURL(result, conference.RoomUrl);
-                //Embeded nie działa z autologinem
-                // resultUrl = _cmClient.GetAutologinURL(result, conference.EmbedRoomUrl);
+                var rooms = await _cmClient.GetConferenceRooms(ConferenceStatus.Active, 1);                
+                var room = rooms.Where(n => n.Name.Equals(meetingName)).FirstOrDefault();
 
+                if (room != null)
+                {
+                    // z room można wyciagnąć Id, Password
+                    var result = await _cmClient.GetAutologinHash(room, email, username);
+
+                    //mały sukces TEN URL MOŻNA PODAĆ JAKO IFRAME !!!!!!!!!!!!!!! :)
+                    //elegancko :)
+                    resultUrl = _cmClient.GetAutologinURL(result, room.RoomUrl);
+                }
+                else
+                {
+                    throw new Exception($"Nie można otworzyć pokoju webinara {meetingName} :(");
+                }
             }
             catch (Exception ex)
-            { _logger.LogError("Message", ex); }
+            { 
+                _logger.LogError("Message", ex); 
+            }
 
             return View("Index", resultUrl);
         }
