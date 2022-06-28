ca1c9b2d18a452656fab81e8f4cec657f238147e
robert845 <108308612+robert845@users.noreply.github.com>
2022-06-28 23:15:22 +0200

    Drobne zmiany

--- ClickMeeting/ClickMeeting/ClickMeeting/Client/IClickMeetingApiClient.cs
+++ ClickMeeting/ClickMeeting/ClickMeeting/Client/IClickMeetingApiClient.cs
@@ -7,8 +7,11 @@ namespace ClickMeeting.ClickMeeting
     public interface IClickMeetingApiClient
     {
         Task<AutologinResult> GetAutologinHash(string roomId, string email, string username, string password, string token);
+        Task<AutologinResult> GetAutologinHash(Room room, string email, string username);
         string GetAutologinURL(AutologinResult autologinHash, string roomURL);
         Task<RoomDetails> GetRoomConferences(string roomId);
-        Task<IEnumerable<Room>> GetConferences(ConferenceStatus status, int page = 1);
+        Task<Room> GetRoom(string roomId);
+        Task<IEnumerable<RoomDetails>> GetConferences(ConferenceStatus status, int page = 1);
+        Task<IEnumerable<Room>> GetConferenceRooms(ConferenceStatus status, int page = 1);
     }
 }
